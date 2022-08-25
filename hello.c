5da980a0ad63fdae1da84822068c7ca6e77e7e76
OwenRobb <111469406+OwenRobb@users.noreply.github.com>
2022-08-25 15:41:28 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Owen Robb
+ *         orobb2@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Owen Robb\n");
+
+  return 0;
+}
