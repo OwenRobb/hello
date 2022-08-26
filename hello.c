e7141ea83ae478bb8a8e736e84d23ba4fce139f6
Caleb Poggemeyer <cpoggemeyer2@huskers.unl.edu>
2022-08-26 21:20:22 +0000

    tempchange

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Owen Robb!\n");
   printf("My major is Computer Science.\n");
+  printf("Caleb Poggemeyer\n");
+  printf("My major is computer engineering.\n");
 
   return 0;
 }
